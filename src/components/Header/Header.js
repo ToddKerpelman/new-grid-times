95295799f21696250b06223a514d5afd9fb64e91
Todd Kerpelman <tkerpelman@plaid.com>
2023-08-15 12:27:40 -0700

    First pass of exercise 1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
@@ -29,7 +29,21 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <InlineGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </InlineGroup>
         <Logo />
+        <InlineGroup>
+          <SubscribeWrapper>
+            <Button>Subscribe</Button>
+            <a href="https://www.google.com/">Already a subscriber?</a>
+          </SubscribeWrapper>
+        </InlineGroup>
       </MainHeader>
     </header>
   );
@@ -39,11 +53,38 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
   display: flex;
   justify-content: space-between;
+`;
+
+const InlineGroup = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    gap: 29px;
+  }
+`;
+
+const SubscribeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  justify-content: center;
+  align-items: center;
+  a {
+    color: var(--color-gray-900);
+    font-size: 14px;
+    font-style: italic;
+    font-weight: 400;
+    text-decoration-line: underline;
+  }
 `;
 
 const ActionGroup = styled.div`
@@ -65,6 +106,10 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+  @media ${QUERIES.laptopAndUp} {
+    justify-content: space-between;
+    margin-top: 8px;
+  }
 `;
 
 export default Header;
