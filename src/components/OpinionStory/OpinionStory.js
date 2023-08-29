a51aa5790ae6924fc8683eea8c1866fdb22e9878
Todd Kerpelman <4397978+ToddKerpelman@users.noreply.github.com>
2023-08-28 17:12:43 -0700

    Line separators and avatars

--- src/components/OpinionStory/OpinionStory.js
+++ src/components/OpinionStory/OpinionStory.js
@@ -1,22 +1,49 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
+import { QUERIES } from "../../constants";
 
 const OpinionStory = ({ id, title, author, avatar }) => {
   return (
-    <a href={`/story/${id}`}>
-      <Wrapper>
-        <Avatar alt="" src={avatar} />
-        <div>
-          <AuthorName>{author}</AuthorName>
-          <ArticleTitle>{title}</ArticleTitle>
-        </div>
-      </Wrapper>
-    </a>
+    <PaddingWrapper>
+      <a href={`/story/${id}`}>
+        <Wrapper>
+          <Avatar alt="" src={avatar} />
+          <AuthorInfo>
+            <AuthorName>{author}</AuthorName>
+            <ArticleTitle>{title}</ArticleTitle>
+          </AuthorInfo>
+        </Wrapper>
+      </a>
+    </PaddingWrapper>
   );
 };
 
+const PaddingWrapper = styled.div`
+  padding-top: 16px;
+  padding-bottom: 16px;
+  background-color: var(--color-gray-100);
+  &:first-child {
+    padding-top: 0px;
+  }
+
+  &:last-child {
+    padding-bottom: 0px;
+  }
+`;
+
 const Wrapper = styled.article`
   color: var(--color-gray-900);
+  display: grid;
+  gap: 12px;
+  grid-template-areas: "authorInfo avatar";
+  align-items: center;
+
+  @media ${QUERIES.tabletOnly} {
+    grid-template-areas:
+      "avatar"
+      "authorInfo";
+    justify-content: left;
+  }
 `;
 
 const Avatar = styled.img`
@@ -25,6 +52,18 @@ const Avatar = styled.img`
   height: 48px;
   border-radius: 50%;
   object-fit: cover;
+  grid-area: avatar;
+  justify-self: end;
+
+  @media ${QUERIES.tabletOnly} {
+    justify-self: left;
+  }
+`;
+
+const AuthorInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  grid-area: authorInfo;
 `;
 
 const AuthorName = styled.p`
