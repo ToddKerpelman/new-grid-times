67cbc619cc728471d692052299f4248f3bfbf0d9
Todd Kerpelman <4397978+ToddKerpelman@users.noreply.github.com>
2023-09-06 16:45:24 -0700

    First pass of SSG

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { MARKET_DATA, SPORTS_STORIES } from '../../data';
+import { MARKET_DATA, SPORTS_STORIES } from "../../data";
 
-import MarketCard from '../MarketCard';
-import SectionTitle from '../SectionTitle';
-import MiniStory from '../MiniStory';
+import MarketCard from "../MarketCard";
+import SectionTitle from "../SectionTitle";
+import MiniStory from "../MiniStory";
+import { QUERIES } from "../../constants";
 
 const SpecialtyStoryGrid = () => {
   return (
@@ -13,8 +14,8 @@ const SpecialtyStoryGrid = () => {
       <MarketsSection>
         <SectionTitle
           cornerLink={{
-            href: '/markets',
-            content: 'Visit Markets data »',
+            href: "/markets",
+            content: "Visit Markets data »",
           }}
         >
           Markets
@@ -28,17 +29,19 @@ const SpecialtyStoryGrid = () => {
       <SportsSection>
         <SectionTitle
           cornerLink={{
-            href: '/sports',
-            content: 'Visit Sports page »',
+            href: "/sports",
+            content: "Visit Sports page »",
           }}
         >
           Sports
         </SectionTitle>
-        <SportsStories>
-          {SPORTS_STORIES.map((data) => (
-            <MiniStory key={data.id} {...data} />
-          ))}
-        </SportsStories>
+        <SportsStoryWrapper>
+          <SportsStories>
+            {SPORTS_STORIES.map((data) => (
+              <MiniStory key={data.id} {...data} />
+            ))}
+          </SportsStories>
+        </SportsStoryWrapper>
       </SportsSection>
     </Wrapper>
   );
@@ -51,10 +54,31 @@ const Wrapper = styled.div`
 
 const MarketsSection = styled.section``;
 
-const MarketCards = styled.div``;
+const MarketCards = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+`;
 
-const SportsSection = styled.section``;
+const SportsSection = styled.section`
+  @media ${QUERIES.tabletAndUp} {
+    max-width: 100%;
+    overflow-x: auto;
+  }
+`;
 
-const SportsStories = styled.div``;
+const SportsStoryWrapper = styled.div`
+  overflow-x: auto;
+}`;
+
+const SportsStories = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+
+  @media ${QUERIES.tabletAndUp} {
+    grid-template-columns: repeat(${SPORTS_STORIES.length}, 200px);
+  }
+`;
 
 export default SpecialtyStoryGrid;
